1ec7e4083c01c65504dd4dfe6f376906a3230085
felipe campos <fcviannaperes@gmail.com>
2025-09-04 08:35:30 -0300

    ajustando o botão de participar

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,11 +6,10 @@ const Home = () => {
       <div className="flex-1 flex-col items-center md:items-start text-center md:text-left mb-8 md:mb-0 mr-16">
         <p className="text-lg text-gray-400">Loja de Carros</p>
         <h4 className="text-4xl sm:text-5xl md:text-6xl font-bold mt-2">O melhor Carro<br/>de Corrida</h4>
-        
-      </div>
-      <button className="mt-8 py-3 px-6 text-white font-extrabold bg-blue-400">
+        <button className="mt-8 py-3 px-6 rounded-4xl text-white font-extrabold bg-blue-400">
         Participar
-      </button>
+        </button>
+      </div>
       <div className="flex-1 items-center justify-center">
         <img src={imagem} className="w-full max-w-sm md:max-w-md"/>
       </div>
